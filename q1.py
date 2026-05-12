d796543b9fe6062f28b6fc4973784f18c382e7e7
alperenbagci66 <abagci24@ku.edu.tr>
2026-05-13 01:48:32 +0300

    prelab11
    prelab11

--- q1.py
+++ q1.py
@@ -1,3 +1,36 @@
 def print_operation(num1, num2, operation):
     # TODO: Your code is here
+    islem = ["+","-","*","/"]
+    if operation not in islem:
+        raise ValueError("Unsupported operation. Use '+', '-', '*', or '/'.")
+        
+    
+    
+    try:
+        num11 = int(num1)
+        num22 = int(num2)
+    
+    except ValueError:
+        print("Input strings must be convertible to integers.")
+    
+    
+    
+    if operation == '/' and num22 == 0:
+        raise ZeroDivisionError("Division by zero is not allowed.")
+        
+        
+        
+    
+    
+    if operation == '+':
+        result = num11 + num22
+    elif operation == '-':
+        result = num11 - num22
+    elif operation == '*':
+        result = num11 * num22
+    elif operation == '/':
+        result = num11 / num22
+    
+    
+
     return f"{num1} {operation} {num2} = {result}"
--- q4.py
+++ q4.py
@@ -1,2 +1,30 @@
+class TabFileValidationError(Exception):
+    pass
+
+
 def validate_tab_file(file_path, expected_fields, required_pattern):
-    return
+    
+    try:
+        with open(file_path,"r") as fid:
+            tt = True
+            count = 0
+            for line in fid:
+                satır = line.strip().split("\t")
+                sayı1 = len(satır)
+                count += 1
+
+                if sayı1 != expected_fields:
+                    raise TabFileValidationError( f"File path: {file_path}, Line {count}: Expected {expected_fields} fields, found {sayı1}")
+                if required_pattern in line:
+                    tt = False
+            
+            if tt:
+                raise TabFileValidationError("File path: data.txt, No field contains required pattern 'missing_pattern'.")
+            
+        return "Tab-delimited file validation passed."
+                
+    except FileNotFoundError:
+        return "File not found."
+    
+    
+
